cc10785d3477e70a233e40cb48be7f813ad9cbbe
Chiu <chiu.y.kwok@mail.mcgill.ca>
2017-10-16 13:37:57 -0400

    created constructors

--- script.js
+++ script.js
@@ -15,40 +15,103 @@
 // - Next or Previous song will move with song index (array style?)
 // 	clicking next will change song status to stop and play for the next one
 
-class song {
 
-	constructor(){ //incl artist and title?
-		this.$title = $("<div>").addClass(".list-title").html(); // need something in the html...
-		this.artist = ;
-		this.index = []; //???
-		this.songStatus = ; // to default to stop?
-	}
+// TO-DO
+// find a way to edit/create song title and artist on the function
+// register event when buttons are being clicked (i.e clicked! + register artist name and song title)
 
+// $(".songIcon", newSong)  ->  returns ALL elements of class "songIcon" which are CHILDREN to the "newSong" element.
+
+
+function createSongTemplate( artist, songTitle ) {
+	var songTemplate = $("#song-list-template").clone();
+	songTemplate.show();
+	$(".panel-body").append(songTemplate);
+	$(".list-title:last").append(songTitle);
+	$(".list-artist:last").append(artist);
+
+	$( ".col-md-1", songTemplate ).click(function() {
+
+		var clickSong = $(".list-title", $(this).next()).text();
+		var clickArtist = $(".list-artist", $(this).next()).text();
+
+	 	//updates controller details
+	 	var controllerTemplate = $("#controller-template").clone();
+	 	$(".control-title").text(clickSong);
+	 	$(".control-artist").text(clickArtist);
+	 	$(".glyphicon-play:last").addClass("glyphicon-pause").removeClass("glyphicon-play")
+
+	 	//this changes the song list icon
+	 	$( ".glyphicon-play", songTemplate ).addClass("glyphicon-pause").removeClass("glyphicon-play")
+
+	});
 }
 
 class musicPlayer {
 
-	songStatus() {
-		//default to stop - show play icon
-		//if play - show play icon
-			//if .click on play status => update status to pause
-			//show pause icon
+	constructor(){
+		this.songList = [];
+		this.currentSongIndex = -1;//if between 0 and songlist.length, then song is being played, otherwise nothing is being selected
+		// in reference to the footer
+		// using sentinel value to know that 
+		this.isCurrentSongPlaying = false; //convention to start boolean questions with 'is'
 	}
 
-	//instance of song
+	addSong(artistName, songName, songURL){
+		let newSong = new song(artistName, songName, songURL);
+		this.songList.push(newSong);
+		let songIndex = this.songList.push(newSong) - 1; // here .push(newSong) acts like .length
+		this.updateView(songIndex);
+	}
+
+	updateView(songIndex){
+		var song = this.songList[songIndex];
+		var songName = song.title;
+		var artistName = song.artist;
+
+		var songTemplate = $("#song-list-template").clone();
+		
+		songTemplate.show();
+		$(".panel-body").append(songTemplate);
+		$(".list-title:last").append(songName);
+		$(".list-artist:last").append(artistName);
+
+		// another option would be to remove all songs (with .remove) and loop through songList array to add back all the songs
+
+	} 
 
 }
 
-// within musicPlayer construct we'll have
-// var song = new Song kinda thing going on
+
+class song {
+
+	constructor(artistName, songName, songURL){
+		this.title = songName;
+		this.artist = artistName;
+		this.url = songURL;
+	}
+
+}
+
 
 $(document).ready(function(){
 
-list of songs here
-maybe like add song or just through constructor
+	let mpInstance = new musicPlayer;
+
+	mpInstance.addSong("artist", "song", "url");
+	mpInstance.addSong("artist1", "song1", "url1");
+
 
 });
 
+
+//HELPFUL LINKS//
+
+// https://stackoverflow.com/questions/25385598/how-to-add-one-specific-html-block-with-jquery
+// https://www.joezimjs.com/javascript/javascript-templating-adding-html-the-right-way/
+
+
+//INSTRUCTIONS//
 
 // A list of songs with their names, artists, and sound URLs (the URLs will not be displayed, but are necessary for audio playback).
 // The user can start playing a track.
